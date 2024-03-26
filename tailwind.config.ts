aeae9349976bc8f8260689b604fbf9b9e1919c63
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2024-03-26 12:47:12 -0400

    formatting cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config = {
   content: [
-	"./pages/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
