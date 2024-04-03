06cad9029ba9a30f563a0c7f4fad04d32e5a8fb5
andrewtlu <andrew.lu@emory.edu>
2024-04-03 14:22:56 -0400

    Add links, svg

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,38 +1,44 @@
 import Link from "next/link";
+import { SearchSVG } from "./svg";
 
+// todo: select active link with bg-gray-700 and text-gray-100
 export default function NavBar() {
     return (
-        <nav className="flex items-center justify-center p-6 gap-x-20">
-            <div>
-                <img src="/logo.png" alt="logo" className="h-full w-full" />
-            </div>
-            <ul className="flex gap-10">
+        <nav className="flex w-full h-32 py-5 items-center justify-center gap-x-20 bg-gray-200">
+            <img src="/logo.png" alt="logo" className="scale-50" />
+            <ul className="flex gap-10 whitespace-pre">
                 <li>
-                    <Link href="/">
+                    <Link href="/" className="bg-white px-5 py-3 rounded-full transition-all font-semibold hover:bg-gray-100">
                         Dashboard
                     </Link>
                 </li>
                 <li>
-                    <Link href="/">
+                    <Link href="/" className="bg-white px-5 py-3 rounded-full transition-all font-semibold hover:bg-gray-100">
                         HR Services
                     </Link>
                 </li>
                 <li>
-                    <Link href="/">
+                    <Link href="/" className="bg-white px-5 py-3 rounded-full transition-all font-semibold hover:bg-gray-100">
                         Events & Announcements
                     </Link>
                 </li>
                 <li>
-                    <Link href="/">
+                    <Link href="/" className="bg-white px-5 py-3 rounded-full transition-all font-semibold hover:bg-gray-100">
                         Employee Directory
                     </Link>
                 </li>
                 <li>
-                    <Link href="/">
+                    <Link href="/" className="bg-white px-5 py-3 rounded-full transition-all font-semibold hover:bg-gray-100">
                         Feedback
                     </Link>
                 </li>
             </ul>
+            
+            <div className="flex gap-5 items-center justify-center text-gray-500">
+                <SearchSVG />
+                <div className="rounded-full w-[48px] h-[48px] bg-gray-500"></div>
+            </div>
+            
         </nav>
     );
 }
