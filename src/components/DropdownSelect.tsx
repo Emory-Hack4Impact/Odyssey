21e5a42c95b2f314c6c7f524e84319a549db34cf
zoie peng <zoiepng3102@gmail.com>
2024-04-04 21:42:22 -0400

    add required and className as optional properties to dropdownSelect component

--- src/components/DropdownSelect.tsx
+++ src/components/DropdownSelect.tsx
@@ -2,12 +2,16 @@ interface Props {
   options: string[];
   placeholder: string;
   id: string;
+  required?: boolean;
+  className?: string;
 }
 
 export default function DropdownSelect(props: Props) {
+  const required = props.required ? props.required : false;
+
   return (
     <div>
-      <select id={props.id} className="text-black">
+      <select id={props.id} className={`text-black ${props.className}`} required={required}>
         <option key={-1} value="" selected hidden>
           {props.placeholder}
         </option>
