04dd4fc406e220a3e2b3cc67f103a51da3c9b85c
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2024-04-14 10:17:01 -0700

    Seed 3 Users (#2)
    * Seed supabase to create users in the auth table; seed prisma to create user metadata at seeded supabase users
    
    * Seed supabase and prisma for remaining users
    
    * ran prettier
    
    forgot to do this before previous commit
    
    ---------
    
    Co-authored-by: Mei Deng <meideng06@gmail.com>

---
+++ supabase/seed.sql
@@ -0,0 +1,81 @@
+INSERT INTO
+  auth.users (
+  instance_id,
+  id,
+  aud,
+  role,
+  email,
+  encrypted_password,
+  email_confirmed_at,
+  recovery_sent_at,
+  last_sign_in_at,
+  raw_app_meta_data,
+  raw_user_meta_data,
+  created_at,
+  updated_at,
+  confirmation_token,
+  email_change,
+  email_change_token_new,
+  recovery_token
+  ) values
+  -- Manager
+  ('00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000',
+  'authenticated',
+  'authenticated',
+  'manager1@example.com',
+  crypt ('password123', gen_salt ('bf')),
+  current_timestamp, current_timestamp, current_timestamp,
+  '{"provider":"email","providers":["email"]}', '{}',
+  current_timestamp, current_timestamp,
+  '', '', '', ''),
+
+  -- Regular user #1
+  ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000001',
+  'authenticated',
+  'authenticated',
+  'user1@example.com',
+  crypt ('password123', gen_salt ('bf')),
+  current_timestamp, current_timestamp, current_timestamp,
+  '{"provider":"email","providers":["email"]}', '{}',
+  current_timestamp, current_timestamp,
+  '', '', '', ''),
+  
+  -- Regular user #2
+  ('00000000-0000-0000-0000-000000000002', '00000000-0000-0000-0000-000000000002',
+  'authenticated',
+  'authenticated',
+  'user2@example.com',
+  crypt ('password123', gen_salt ('bf')),
+  current_timestamp, current_timestamp, current_timestamp,
+  '{"provider":"email","providers":["email"]}', '{}',
+  current_timestamp, current_timestamp,
+  '', '', '', ''),
+  
+  -- HR user
+  ('00000000-0000-0000-0000-000000000003', '00000000-0000-0000-0000-000000000003',
+  'authenticated',
+  'authenticated',
+  'hr@example.com',
+  crypt ('password123', gen_salt ('bf')),
+  current_timestamp, current_timestamp, current_timestamp,
+  '{"provider":"email","providers":["email"]}', '{}',
+  current_timestamp, current_timestamp,
+  '', '', '', '');
+
+-- test user email identities
+INSERT INTO
+  auth.identities (
+  id,
+  user_id,
+  provider_id,
+  identity_data,
+  provider,
+  last_sign_in_at,
+  created_at,
+  updated_at
+  ) (
+  SELECT uuid_generate_v4 (), id, id,
+    format('{"sub":"%s","email":"%s"}', id :: text, email) :: jsonb, 'email',
+    current_timestamp, current_timestamp, current_timestamp
+  FROM auth.users
+  );
