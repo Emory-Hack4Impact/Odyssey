5feffcf0651c3312c5d9feafe641871b289b1dff
andrewtlu <andrew.lu@emory.edu>
2024-04-16 12:26:10 -0400

    Implement submitFormData function

--- src/app/dashboard/time-off-req/api.tsx
+++ src/app/dashboard/time-off-req/api.tsx
@@ -2,10 +2,23 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function submitFormData(formData: any) {
+export interface TimeOffRequest {
+  leaveType: string;
+  otherLeaveType: string;
+  startDate: string;
+  endDate: string;
+  comments: string;
+}
+
+export async function submitFormData(formData: TimeOffRequest) {
   return await prisma.timeOffRequest.create({
     data: {
-      ...formData,
-    },
+      id: "00000000-0000-0000-0000-000000000001",
+      leaveType: formData.leaveType,
+      otherLeaveType: formData.otherLeaveType,
+      startDate: formData.startDate,
+      endDate: formData.endDate,
+      comments: formData.comments,
+    }
   });
 }
