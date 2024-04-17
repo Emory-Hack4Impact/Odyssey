f5608f2c0d63095d7a4690c577f51349ee8e5aaf
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2024-04-17 13:48:50 -0400

    convert POST to server actions

--- src/app/api/time-off-req/index.ts
+++ src/app/api/time-off-req/index.ts
@@ -1,30 +1,26 @@
-import { PrismaClient } from '@prisma/client';
-import { FormData } from '../../dashboard/time-off-req/TimeOffForm';
-import { NextApiRequest, NextApiResponse } from 'next';
+"use server";
 
-const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
 
-// TODO: currently broken, need to check w/ Rafael if routing is correct
-export async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const formData: FormData = req.body;
-  console.log("Submitting formdata, content: " + formData);
-  try {
-    const result = await createRequest(formData);
-    return res.status(200).json(result);
-  } catch (error) {
-    return res.status(500).json({ error: 'Error creating time off request' });
-  }
+export interface SubmitTimeOffRequest {
+  id: string;
+  leaveType: string;
+  otherLeaveType: string;
+  startDate: string;
+  endDate: string;
+  comments: string;
 }
 
-export async function createRequest(formData: FormData) {
+export async function SubmitTimeOff(data: SubmitTimeOffRequest) {
+  const prisma = new PrismaClient();
   return await prisma.timeOffRequest.create({
     data: {
-      employeeId: "00000000-0000-0000-0000-000000000001", // hard coded for now, need to fetch from session in practice
-      leaveType: formData.leaveType,
-      otherLeaveType: formData.otherLeaveType,
-      startDate: formData.startDate,
-      endDate: formData.endDate,
-      comments: formData.comments,
-    }
+      employeeId: data.id,
+      leaveType: data.leaveType,
+      otherLeaveType: data.otherLeaveType,
+      startDate: data.startDate,
+      endDate: data.endDate,
+      comments: data.comments,
+    },
   });
 }
--- src/app/dashboard/time-off-req/TimeOffForm.tsx
+++ src/app/dashboard/time-off-req/TimeOffForm.tsx
@@ -1,4 +1,8 @@
-import React, { useState } from 'react';
+import {
+  SubmitTimeOff,
+  type SubmitTimeOffRequest,
+} from "@/app/api/time-off-req";
+import React, { useState } from "react";
 
 export interface FormData {
   leaveType: string;
@@ -9,33 +13,39 @@ export interface FormData {
 }
 
 const TimeOffForm: React.FC = () => {
-  const [formData, setFormData] = useState<FormData>({
-    leaveType: '',
-    otherLeaveType: '',
-    startDate: '',
-    endDate: '',
-    comments: ''
+  const [formData, setFormData] = useState<SubmitTimeOffRequest>({
+    id: "00000000-0000-0000-0000-000000000001",
+    leaveType: "",
+    otherLeaveType: "",
+    startDate: "",
+    endDate: "",
+    comments: "",
   });
 
-  const [showOtherLeaveTypeField, setShowOtherLeaveTypeField] = useState<boolean>(false);
+  const [showOtherLeaveTypeField, setShowOtherLeaveTypeField] =
+    useState<boolean>(false);
   const [formErrors, setFormErrors] = useState<Partial<FormData>>({});
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >,
+  ) => {
     const { name, value } = e.target;
 
-    if (name === 'leaveType' && value === 'Other') {
+    if (name === "leaveType" && value === "Other") {
       setShowOtherLeaveTypeField(true);
-    } else if (name === 'leaveType' && value !== 'Other') {
+    } else if (name === "leaveType" && value !== "Other") {
       setShowOtherLeaveTypeField(false);
-      setFormData(prevState => ({
+      setFormData((prevState) => ({
         ...prevState,
-        otherLeaveType: ''
+        otherLeaveType: "",
       }));
     }
 
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
@@ -44,117 +54,128 @@ const TimeOffForm: React.FC = () => {
     const errors: Partial<FormData> = {};
 
     if (!formData.leaveType) {
-      errors.leaveType = '*required';
+      errors.leaveType = "*required";
     }
 
     if (!formData.startDate) {
-      errors.startDate = '*required';
+      errors.startDate = "*required";
     }
 
     if (!formData.endDate) {
-      errors.endDate = '*required';
+      errors.endDate = "*required";
     }
 
     setFormErrors(errors);
 
-    // TODO: returns 404 atm, routing is incorrect in /api/time-off-req/
     if (Object.keys(errors).length === 0) {
       console.log(formData);
-      // Add submission logic here
-      try {
-        const response = await fetch('/api/time-off-req', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(formData)
-        });
-
-        if (!response.ok) {
-          console.log(response.status);
-          throw new Error('Error submitting time off request');
-        }
-
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error('Error submitting time off request', error);
-      }
+      // TODO: do something on success
+      await SubmitTimeOff(formData)
+        .then((r) =>
+          console.log(`successfully submitted time off request: ${r.id}`),
+        )
+        .catch((e) => console.error(e));
     }
   };
 
   return (
-    <div className="mx-auto mt-10 w-full max-w-2xl p-6 border border-gray-200 rounded-lg shadow-lg bg-white">
+    <div className="mx-auto mt-10 w-full max-w-2xl rounded-lg border border-gray-200 bg-white p-6 shadow-lg">
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
-          <label htmlFor="leaveType" className="block text-gray-700 text-lg">Leave Type:</label>
+          <label htmlFor="leaveType" className="block text-lg text-gray-700">
+            Leave Type:
+          </label>
           <select
             id="leaveType"
             name="leaveType"
             value={formData.leaveType}
             onChange={handleChange}
-            className="form-select mt-1 block w-full rounded-md border border-gray-300 text-gray-700 text-lg p-2"
+            className="form-select mt-1 block w-full rounded-md border border-gray-300 p-2 text-lg text-gray-700"
           >
             <option value="">Select Leave Type</option>
             <option value="Annual Leave">Annual Leave</option>
             <option value="Sick Leave">Sick Leave</option>
             <option value="Bereavement">Bereavement</option>
-            <option value="Maternity/Paternity Leave">Maternity/Paternity Leave</option>
+            <option value="Maternity/Paternity Leave">
+              Maternity/Paternity Leave
+            </option>
             <option value="Jury Duty">Jury Duty</option>
             <option value="Optional Holiday">Optional Holiday</option>
             <option value="Other">Other</option>
           </select>
-          {formErrors.leaveType && <p className="text-red-500 text-sm">{formErrors.leaveType}</p>}
+          {formErrors.leaveType && (
+            <p className="text-sm text-red-500">{formErrors.leaveType}</p>
+          )}
         </div>
         {showOtherLeaveTypeField && (
           <div className="mb-4">
-            <label htmlFor="otherLeaveType" className="block text-gray-700 text-lg">Other Leave Type:</label>
+            <label
+              htmlFor="otherLeaveType"
+              className="block text-lg text-gray-700"
+            >
+              Other Leave Type:
+            </label>
             <input
               type="text"
               id="otherLeaveType"
               name="otherLeaveType"
               value={formData.otherLeaveType}
               onChange={handleChange}
-              className="form-input mt-1 block w-full rounded-md border border-gray-300 text-gray-700 text-lg p-2"
+              className="form-input mt-1 block w-full rounded-md border border-gray-300 p-2 text-lg text-gray-700"
             />
           </div>
         )}
         <div className="mb-4">
-          <label htmlFor="startDate" className="block text-gray-700 text-lg">Start Date:</label>
+          <label htmlFor="startDate" className="block text-lg text-gray-700">
+            Start Date:
+          </label>
           <input
             type="date"
             id="startDate"
             name="startDate"
             value={formData.startDate}
             onChange={handleChange}
-            className="form-input mt-1 block w-full rounded-md border border-gray-300 text-gray-700 text-lg p-2"
+            className="form-input mt-1 block w-full rounded-md border border-gray-300 p-2 text-lg text-gray-700"
           />
-          {formErrors.startDate && <p className="text-red-500 text-sm">{formErrors.startDate}</p>}
+          {formErrors.startDate && (
+            <p className="text-sm text-red-500">{formErrors.startDate}</p>
+          )}
         </div>
         <div className="mb-4">
-          <label htmlFor="endDate" className="block text-gray-700 text-lg">End Date:</label>
+          <label htmlFor="endDate" className="block text-lg text-gray-700">
+            End Date:
+          </label>
           <input
             type="date"
             id="endDate"
             name="endDate"
             value={formData.endDate}
             onChange={handleChange}
-            className="form-input mt-1 block w-full rounded-md border border-gray-300 text-gray-700 text-lg p-2"
+            className="form-input mt-1 block w-full rounded-md border border-gray-300 p-2 text-lg text-gray-700"
           />
-          {formErrors.endDate && <p className="text-red-500 text-sm">{formErrors.endDate}</p>}
+          {formErrors.endDate && (
+            <p className="text-sm text-red-500">{formErrors.endDate}</p>
+          )}
         </div>
         <div className="mb-4">
-          <label htmlFor="comments" className="block text-gray-700 text-lg">Additional Information:</label>
+          <label htmlFor="comments" className="block text-lg text-gray-700">
+            Additional Information:
+          </label>
           <textarea
             id="comments"
             name="comments"
             value={formData.comments}
             onChange={handleChange}
-            className="form-textarea mt-1 block w-full rounded-md border border-gray-300 text-gray-700 text-lg p-2"
+            className="form-textarea mt-1 block w-full rounded-md border border-gray-300 p-2 text-lg text-gray-700"
             rows={4}
           />
         </div>
-        <button type="submit" className="bg-blue-500 text-white text-lg px-4 py-2 rounded hover:bg-blue-600">Submit</button>
+        <button
+          type="submit"
+          className="rounded bg-blue-500 px-4 py-2 text-lg text-white hover:bg-blue-600"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
