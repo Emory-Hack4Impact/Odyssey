6e1e94ffd46d70caaa288a4c4cb5940ef74774c2
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2024-04-30 11:27:55 -0700

    Perf eval form (#6)
    * create dropdown selector component
    
    * format dropdown component file
    
    * create page for performance eval form
    
    * add id prop for dropdown component
    
    * add required and className as optional properties to dropdownSelect component
    
    * move form to component
    
    * add functionality to form dropdowns
    
    * utilize FormData interface
    
    * add year field
    
    * add upper bound for year field
    
    * format dropdowns
    
    * fix selected warning
    
    * add comment fields
    
    * add form text
    
    * add hover effect on submit button
    
    * add e-signature field (placeholder)
    
    * prettier
    
    * Delete .env
    
    No no
    
    ---------
    
    Co-authored-by: zoie peng <zoiepng3102@gmail.com>
    Co-authored-by: Marcus Hill <95888094+MarcusCHill@users.noreply.github.com>

--- src/components/DropdownSelect.tsx
+++ src/components/DropdownSelect.tsx
@@ -1,13 +1,25 @@
 interface Props {
   options: string[];
   placeholder: string;
+  id: string;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  required?: boolean;
+  className?: string;
 }
 
 export default function DropdownSelect(props: Props) {
+  const required = props.required ? props.required : false;
+
   return (
     <div>
-      <select className="text-black">
-        <option key={-1} value="" selected hidden>
+      <select
+        id={props.id}
+        className={`text-black ${props.className}`}
+        required={required}
+        defaultValue={""}
+        onChange={props.onChange}
+      >
+        <option key={-1} value="" className="hidden">
           {props.placeholder}
         </option>
         {props.options.map((option, index) => {
