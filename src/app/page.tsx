29d2f8e099e4417583268254a0eadd59b51d4904
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2024-04-30 15:31:24 -0400

    clean up boilerplate code from supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,16 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
+import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
 import { SubNavBar } from "@/components/NavBar";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Header from "@/components/Header";
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
-
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-20">
       <NavBar />
-      {/* restrict to width of content */}
       <SubNavBar selected="HR Services for Jane" />
-      {/* <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
-        <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav> */}
-
       <div className="animate-in flex max-w-4xl flex-1 flex-col gap-20 px-3 opacity-0">
-        <Header />
-        <main className="flex flex-1 flex-col gap-6">
-          <h2 className="mb-4 text-4xl font-bold">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
+        <main className="flex flex-1 flex-col gap-6"></main>
       </div>
-
-      <footer className="flex w-full justify-center border-t border-t-foreground/10 p-8 text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 }
