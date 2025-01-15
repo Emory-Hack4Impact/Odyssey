f4ba28bc4ea497e722b49ec140da2f2639313267
Andrew Lu <50790681+andrewtlu@users.noreply.github.com>
2025-01-14 20:04:38 -0500

    Add forgot password functionality (#11)
    * Set up UI for forgot and reset passwords
    
    * Add supabase as a dev dependency (not necessary, can revert in the future)
    
    * Add debug print and psql login info
    
    * Remove supabase as deb dependency (favor local existing install)
    
    * Fix supabase auth seed (instance_id refers to supabase auth instance :) )
    
    * Move submit button to components, begin reworking forgot-password & reset-password
    
    * Fix email redirect, add configuration/env variable
    
    * Fix reset password logic
    
    ---------
    
    Co-authored-by: Albert Terc <albytterc@protonmail.com>

---
+++ src/app/reset-password/updatePassword.ts
@@ -0,0 +1,35 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+const updatePassword = async (formData: FormData) => {
+  "use server";
+
+  const newPassword = formData.get("newPassword") as string;
+  const confirmPassword = formData.get("confirmPassword") as string;
+  const accessToken = formData.get("accessToken") as string;
+  const refreshToken = formData.get("refreshToken") as string;
+  const supabase = createClient();
+
+  await supabase.auth.setSession({
+    access_token: accessToken,
+    refresh_token: refreshToken,
+  })
+
+  if (newPassword !== confirmPassword) {
+    console.log("passwords must match");
+    return redirect("/reset-password?message=Passwords must match");
+  }
+
+  const { error } = await supabase.auth.updateUser({
+    password: confirmPassword,
+  });
+
+  if (error) {
+    console.log(error);
+    return redirect(`/forgot-password?message=${error.message}`);
+  } else {
+    return redirect("/protected");
+  }
+};
+
+export default updatePassword;
