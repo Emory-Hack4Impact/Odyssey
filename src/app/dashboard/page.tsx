eb5ee49c96a6ea9fb6970219e5be9e084eecda82
andrewtlu <andrew.lu@emory.edu>
2025-01-17 17:13:55 -0500

    Add automatic redirect to dashboard from home page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,21 @@
 import { SubNavBar } from "@/components/NavBar";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export default async function Index() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-20">
       <div className="animate-in flex max-w-4xl flex-1 flex-col gap-20 px-3 opacity-0">
-        <SubNavBar selected="HR Services for Jane" />
+        <SubNavBar selected={`HR Services for ${user.email}`} />
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { SubNavBar } from "@/components/NavBar";
+import { redirect } from "next/navigation";
 
 export default async function Index() {
-  return (
-    <div className="flex w-full flex-1 flex-col items-center gap-20">
-      <div className="animate-in flex max-w-4xl flex-1 flex-col gap-20 px-3 opacity-0">
-        <SubNavBar selected="HR Services for Jane" />
-      </div>
-    </div>
-  );
+  redirect("/dashboard");
 }
