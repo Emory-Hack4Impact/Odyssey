0184d097f4fb1dfaf580ec495c50e64359f199fd
andrewtlu <andrew.lu@emory.edu>
2025-03-03 20:20:58 -0500

    Use headless UI tabs for HRServices

--- src/app/dashboard/hrservices/HRServices.tsx
+++ src/app/dashboard/hrservices/HRServices.tsx
@@ -1,95 +1,80 @@
 "use client";
 import PerfEval from "@/components/performance-evaluation/PerfEval";
 import TimeOff from "@/components/time-off/TimeOff";
-import { useState } from "react";
+import { Tab } from "@headlessui/react";
+import { Fragment } from "react";
 
 interface HRServicesProps {
   username: string;
 }
 
 export const HRServices = ({ username }: HRServicesProps) => {
-  const [activeTab, setActiveTab] = useState("timeOff");
-
-  const handleTabChange = (tab: string) => {
-    setActiveTab(tab);
-  };
+  const categories = [
+    { key: "timeOff", label: "Time Off", component: <TimeOff /> },
+    {
+      key: "careerDev",
+      label: "Career Development",
+      component: (
+        <div>
+          <h2 className="mb-4 text-lg font-medium">Career Development</h2>
+          {/* Insert Career Dev Component */}
+        </div>
+      ),
+    },
+    {
+      key: "perfEvals",
+      label: "Performance Evaluations",
+      component: <PerfEval />,
+    },
+    {
+      key: "benefits",
+      label: "Benefits",
+      component: (
+        <div>
+          <h2 className="mb-4 text-lg font-medium">Benefits</h2>
+          {/* Insert content here */}
+        </div>
+      ),
+    },
+    {
+      key: "docs",
+      label: "Documents",
+      component: (
+        <div>
+          <h2 className="mb-4 text-lg font-medium">Documents</h2>
+          {/* Insert content here */}
+        </div>
+      ),
+    },
+  ];
 
   return (
-    <div className="mt-12 flex min-h-screen w-full items-start px-4">
-      <div className="flex flex-1 flex-col gap-20 px-3 opacity-0 animate-in">
-        <div className="flex w-full flex-col">
-          <div className="flex justify-between border-b-2">
-            <h1 className="mb-4 text-xl font-semibold">{`HR Services for ${username}`}</h1>
-            <div className="flex">
-              <button
-                className={`px-4 ${activeTab === "timeOff" ? "border-b-2 border-black font-medium transition-all" : ""}`}
-                onClick={() => handleTabChange("timeOff")}
-              >
-                Time Off
-              </button>
-              <button
-                className={`px-4 ${activeTab === "careerDev" ? "border-b-2 border-black font-medium transition-all" : ""}`}
-                onClick={() => handleTabChange("careerDev")}
-              >
-                Career Development
-              </button>
-              <button
-                className={`px-4 ${activeTab === "perfEvals" ? "border-b-2 border-black font-medium transition-all" : ""}`}
-                onClick={() => handleTabChange("perfEvals")}
-              >
-                Performance Evaluations
-              </button>
-              <button
-                className={`px-4 ${activeTab === "benefits" ? "border-b-2 border-black font-medium transition-all" : ""}`}
-                onClick={() => handleTabChange("benefits")}
-              >
-                Benefits
-              </button>
-              <button
-                className={`px-4 ${activeTab === "docs" ? "border-b-2 border-black font-medium transition-all" : ""}`}
-                onClick={() => handleTabChange("docs")}
-              >
-                Documents
-              </button>
-            </div>
-          </div>
-
-          <div className="mt-6 p-4">
-            {activeTab === "timeOff" && (
-              <div>
-                <TimeOff />
-              </div>
-            )}
-
-            {activeTab === "careerDev" && (
-              <div>
-                <h2 className="mb-4 text-lg font-medium">Career Development</h2>
-                {/* Insert Career Dev Component */}
-              </div>
-            )}
-
-            {activeTab === "perfEvals" && (
-              <div>
-                <PerfEval />
-              </div>
-            )}
-
-            {activeTab === "benefits" && (
-              <div>
-                <h2 className="mb-4 text-lg font-medium">Benefits</h2>
-                {/* Insert content here */}
-              </div>
-            )}
-
-            {activeTab === "docs" && (
-              <div>
-                <h2 className="mb-4 text-lg font-medium">Documents</h2>
-                {/* Insert content here */}
-              </div>
-            )}
-          </div>
+    <div className="m-12 flex min-h-screen w-auto flex-col items-start px-4">
+      <Tab.Group>
+        <div className="flex w-full flex-row justify-between gap-20 border-b-2 px-3 opacity-0 animate-in">
+          <h1 className="mb-4 text-xl font-semibold">{`HR Services for ${username}`}</h1>
+          <Tab.List className="flex">
+            {categories.map((category) => (
+              <Tab key={category.key} as={Fragment}>
+                {({ selected }) => (
+                  <button
+                    className={`px-4 focus:outline-none ${selected ? "border-b-2 border-black font-medium transition-all" : ""}`}
+                  >
+                    {/* if we have more time, should try to fix the font-medium shifting the tab buttons slightly */}
+                    {category.label}
+                  </button>
+                )}
+              </Tab>
+            ))}
+          </Tab.List>
         </div>
-      </div>
+        {/* Tab panels */}
+        <Tab.Panels className="mt-6 w-full p-4">
+          {categories.map((category) => (
+            <Tab.Panel key={category.key}>{category.component}</Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 };
