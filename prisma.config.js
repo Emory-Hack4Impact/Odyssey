04470cf68e1d3826c44faae8a83550f34cf73707
mia-oww <miafendi01@gmail.com>
2025-11-09 21:15:28 -0500

    final fix for config

--- prisma.config.js
+++ prisma.config.js
@@ -6,7 +6,7 @@ dotenv.config();
 const config = defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
-    seed: "tsx --env-file=.env prisma/seed.ts",
+    seed: "node -r ts-node/register --env-file=.env prisma/seed.ts",
   },
 });
 
