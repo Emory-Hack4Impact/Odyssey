13b13c3b64607e5d7893e703773ace88239f2466
Sihao <zhangsihao2005@outlook.com>
2025-11-22 21:11:43 -0500

    Update database url config for prisma 7

--- prisma.config.js
+++ prisma.config.js
@@ -1,11 +1,14 @@
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const config = defineConfig({
   schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
   migrations: {
     seed: "tsx --env-file=.env prisma/seed.ts",
   },
