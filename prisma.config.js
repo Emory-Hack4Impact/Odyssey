3ab7cdf3ae5ca25348e4880479ba75921062b7c0
Sihao <zhangsihao2005@outlook.com>
2025-11-29 14:45:25 -0500

    fallback to old url config due to env issue. to be discussed later

--- prisma.config.js
+++ prisma.config.js
@@ -1,14 +1,11 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const config = defineConfig({
   schema: "prisma/schema.prisma",
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
   migrations: {
     seed: "tsx --env-file=.env prisma/seed.ts",
   },
