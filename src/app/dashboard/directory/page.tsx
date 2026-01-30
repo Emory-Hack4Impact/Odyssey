10b846e715a4d9fa9879993be478950a7353eebb
Caleb Jennings <calebbirtch.j@gmail.com>
2026-01-30 10:13:04 -0500

    add directory page to navbar

---
+++ src/app/dashboard/directory/page.tsx
@@ -0,0 +1,28 @@
+"use server";
+
+import Error from "@/components/Error";
+import { prisma } from "@/lib/prisma";
+import { getUser } from "@/utils/supabase/server";
+
+export default async function HRServicesPage() {
+  const user = (await getUser())!;
+
+  // Get UserMetadata from DB
+  const data = await prisma.userMetadata.findUnique({
+    where: { id: user.id },
+  });
+
+  if (!data) return <Error message={`User not found, please try again!\n`} />;
+
+  // Get userId, username and role and pass as props
+  const userId = user?.id ?? user?.user_metadata?.id ?? "0";
+  const username = user?.email ?? user?.user_metadata?.name ?? "User";
+  const userMetadata = data;
+  const userRole = userMetadata?.position ?? "Unknown";
+
+  return (
+    <div>
+      hey baby :/
+    </div>
+  );
+}
